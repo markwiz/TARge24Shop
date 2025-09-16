fcfbe199dcd252d2179f0785f8be4b0bc298abb8
markwiz <mmark.kuusik@gmail.com>
2025-09-16 13:18:09 +0300

    Add files via upload

---
+++ ShopTARge24.Core/ServiceInterface/ISpaceshipServices.cs
@@ -0,0 +1,14 @@
+﻿using ShopTARge24.Core.Domain;
+using ShopTARge24.Core.Dto;
+
+
+namespace ShopTARge24.Core.ServiceInterface
+{
+    public interface ISpaceshipServices
+    {
+        Task<Spaceships> Create(SpaceshipDto dto);
+        Task<Spaceships> DetailAsync(Guid id);
+        Task<Spaceships> Delete(Guid id);
+        Task<Spaceships> Update(SpaceshipDto dto);
+    }
+}
