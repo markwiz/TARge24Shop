d792ecc7b2b3aad57c1146c42f4b90f129e79a3f
markwiz <mmark.kuusik@gmail.com>
2025-09-16 13:21:48 +0300

    Add files via upload

---
+++ TARge24Shop/Program.cs
@@ -0,0 +1,39 @@
+using ShopTARge24.Data;
+using Microsoft.EntityFrameworkCore;
+using ShopTARge24.Core.ServiceInterface;
+using ShopTARge24.ApplicationServices.Services;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddControllersWithViews();
+
+builder.Services.AddScoped<ISpaceshipServices, SpaceshipServices>();
+
+builder.Services.AddDbContext<ShopTARge24Context>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Home/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+app.UseRouting();
+
+app.UseAuthorization();
+
+app.MapStaticAssets();
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}")
+    .WithStaticAssets();
+
+
+app.Run();
