7eac2ca4e96c3c1befe53d7975084e3a51b6b00c
markwiz <mmark.kuusik@gmail.com>
2025-09-16 13:17:42 +0300

    Add files via upload

---
+++ ShopTARge24.Data/ShopTARge24Context.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ShopTARge24.Core.Domain;
+
+
+
+namespace ShopTARge24.Data
+{
+    public class ShopTARge24Context : DbContext
+    {
+        public ShopTARge24Context(DbContextOptions<ShopTARge24Context> options)
+            : base(options) { }
+
+        public DbSet<Spaceships> Spaceships { get; set; }
+    }
+}
